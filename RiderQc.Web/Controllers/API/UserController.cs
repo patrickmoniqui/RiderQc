83985adacf46fca3c8146c0393d6b13653915d69
BEN-PC\Ben <benji.lam@hotmail.ca>
2017-09-25 13:58:33 -0400

    Added validation and implemented the Decode/Encode/HashToSHA256 methods

--- RiderQc.Web/Controllers/API/UserController.cs
+++ RiderQc.Web/Controllers/API/UserController.cs
@@ -1,10 +1,11 @@
-﻿using Newtonsoft.Json;
-using RiderQc.Web.Entities;
+﻿using RiderQc.Web.Entities;
 using RiderQc.Web.Repository.Interface;
 using RiderQc.Web.ViewModels.User;
-using Swashbuckle.Swagger.Annotations;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
 using System.Web.Http;
 using System.Web.Http.Description;
 
@@ -31,14 +32,30 @@ namespace RiderQc.Web.Controllers.API
         [ResponseType(typeof(UserRegisterViewModel))]
         public IHttpActionResult Register(UserRegisterViewModel userViewModel)
         {
-            if(!ModelState.IsValid)
+            if (!ModelState.IsValid)
             {
                 return BadRequest(ModelState);
             }
 
+            string username = Base64Decode(userViewModel.Username);
+
+            if (repo.CheckUserExistence(username))
+                return BadRequest("That username is already taken.");
+
+            userViewModel.Username = username;
+
+            string password = Base64Decode(userViewModel.Password);
+
+            userViewModel.Password = HashToSHA256(password);
+
+            DateTime? userDOB = userViewModel.DateOfBirth;
+            string region = userViewModel.Region;
+            string ville = userViewModel.Ville;
+            string dpURL = userViewModel.DpUrl;
+
             bool result = repo.RegisterUser(userViewModel);
-            
-            if(result)
+
+            if (result)
             {
                 return Ok("User successfully registered!");
             }
@@ -93,5 +110,33 @@ namespace RiderQc.Web.Controllers.API
 
             return Ok(users);
         }
+        #region Methods for Encryption/Decryption
+        public static string Base64Encode(string plainText)
+        {
+            var plainTextBytes = System.Text.Encoding.UTF8.GetBytes(plainText);
+            return System.Convert.ToBase64String(plainTextBytes);
+        }
+
+        public static string Base64Decode(string base64EncodedData)
+        {
+            var base64EncodedBytes = System.Convert.FromBase64String(base64EncodedData);
+            return System.Text.Encoding.UTF8.GetString(base64EncodedBytes);
+        }
+
+        public static String HashToSHA256(String value)
+        {
+            StringBuilder Sb = new StringBuilder();
+
+            using (SHA256 hash = SHA256.Create())
+            {
+                Encoding enc = Encoding.UTF8;
+                byte[] result = hash.ComputeHash(enc.GetBytes(value));
+
+                foreach (byte b in result)
+                    Sb.Append(b.ToString("x2"));
+            }
+            return Sb.ToString();
+        }
+        #endregion
     }
 }
