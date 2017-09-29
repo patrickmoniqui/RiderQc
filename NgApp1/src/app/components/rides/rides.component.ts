cc447eef1d55dfc2e0a63538ed35ec43ee997030
Patrick Moniqui <pat_moniqui@hotmail.com>
2017-09-29 14:05:45 -0400

    Ajout project Angular4

---
+++ NgApp1/src/app/components/rides/rides.component.ts
@@ -0,0 +1,27 @@
+﻿import { Component, OnInit } from '@angular/core';
+import { RiderqcService } from '../../services/riderqc.service';
+
+@Component({
+  selector: 'app-rides',
+  templateUrl: './rides.component.html',
+  styleUrls: ['./rides.component.css']
+})
+export class RidesComponent implements OnInit {
+    public rides: Ride[];
+    constructor(public riderqcSerice: RiderqcService)
+    {
+
+    }
+
+    ngOnInit() {
+        this.riderqcSerice.getRides().subscribe((rides) => {
+            //console.log(rides);
+            this.rides = rides;
+      });
+  }
+}
+
+interface Ride {
+    Arrive: Date;
+    Descript: string;
+}
