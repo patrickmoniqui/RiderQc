454ca344694489ded6808a6a892ad0d44dce4ac8
Patrick Moniqui <pat_moniqui@hotmail.com>
2017-10-02 12:30:42 -0400

    rename angular project.

--- RiderQc.Ng/src/app/app.module.ts
+++ RiderQc.Ng/src/app/app.module.ts
@@ -5,14 +5,14 @@ import { HttpModule } from '@angular/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { RidesComponent } from './components/rides/rides.componen;
-import { TrajetComponent } from './components/trajet/trajet.component't';
+import { RidesComponent } from './components/rides/rides.component';
+import { TrajetComponent } from './components/trajet/trajet.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    RidesCompone,
-    TrajetComponentnt
+    RidesComponent,
+    TrajetComponent
   ],
   imports: [
     BrowserModule,
