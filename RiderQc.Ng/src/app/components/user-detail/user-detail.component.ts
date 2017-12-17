44b109d34f0870985ef126e743ada55d41a71fe0
Patrick Moniqui <pat_moniqui@hotmail.com>
2017-12-17 00:08:27 -0500

    fix user details date of birth format

--- RiderQc.Ng/src/app/components/user-detail/user-detail.component.ts
+++ RiderQc.Ng/src/app/components/user-detail/user-detail.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Subscription } from "rxjs/Subscription";
+import { RouterModule } from '@angular/router';
 
 //Model
 import { User } from '../../model/user';
--- RiderQc.Ng/src/app/components/user-edit/user-edit.component.ts
+++ RiderQc.Ng/src/app/components/user-edit/user-edit.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from "rxjs/Observable";
 import { FormControl } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 import { FormGroup, Validators } from '@angular/forms';
 
 //Model
